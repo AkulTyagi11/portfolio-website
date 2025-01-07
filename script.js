8f75f7c811a2a707657529e70c9c34624e4ac916
Akul Tyagi <akultyagi2304@gmail.com>
2025-01-08 02:54:41 +0530

    Made the site responsive and changed fonts.

---
+++ script.js
@@ -0,0 +1,19 @@
+// sections = document.querySelectorAll('section');
+// navLinks = document.querySelectorAll('header nav a');
+
+// window.onscroll = () => {
+//     sections = forEach(sec => {
+//         top = window.scrollY;
+//         offset = window.offsetTop - 150;
+//         height = window.offsetHeight;
+//         id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height) {
+//             navLinks.forEach(link => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a [href*=' + id + ']'),classList.add('active');
+//             })
+//         }
+//     })
+// }
+
---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'orbi': ['Orbitron', 'sans-serif'],
+        'lex': ['Lexend', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
